609d10525338acc0d1c20bb452b17331bebd79f0
unknown <sampreeti.bm04@gmail.com>
2023-07-19 00:13:44 +0530

    Finishing up

--- js/script.js
+++ js/script.js
@@ -15,3 +15,21 @@ function playpause(){
         pausebtn.style.display = "none";
     }
 }
+
+function onclickbtn() {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("loginForm").reset();
+}
+
+
+function onclickclose(){
+    document.getElementById("myModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    let modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        console.log(event);
+        modal.style.display = "none";
+      }
+}
